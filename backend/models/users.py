402583c9c221875035d3bd60e9eb8597dae2e617
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2025-06-20 23:09:52 +0530

    sign_in demo

--- backend/models/users.py
+++ backend/models/users.py
@@ -13,3 +13,12 @@ def sign_up(email: str, name: str, phone: str, password: str):
     "name": name,
     "password": hashed_password
   }).execute()
+
+def sign_in(email: str, password: str):
+  hashed_password = hash(password)
+  response = supabase_client.table("users").select("*").eq("email", email).single().execute()
+  user = response.data
+  if user and user["password"] == hashed_password:
+    return user
+  else:
+    return None
