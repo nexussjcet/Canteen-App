e4f0bf2403ddcf356dcc291160f16c15990e1a29
Rino Robert <150415679+rinorobert@users.noreply.github.com>
2025-07-05 13:23:38 +0530

    Add FoodCard component, UI card, and beverage images

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
-import Login from "@/components/pages/login"
+import FoodCard from "./components/FoodCard";
+import teaImg from "@/assets/tea.png";
+import coffeeImg from "@/assets/coffee.png";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Login />
+    <div className="min-h-screen bg-[#F9FAFB] p-4 flex flex-col gap-4 items-center">
+      <FoodCard title="Tea" price={10} image={teaImg} />
+      <FoodCard title="Coffee" price={15} image={coffeeImg} />
     </div>
-  )
+  );
 }
-
-export default App
